9aaf6f747de65a1a72384f476fb05b22a4e43087
Srinidhi-Ravi <srini3998@gmailcom>
2020-12-21 15:09:32 +0530

    mul method added

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -8,11 +8,15 @@ public class Calc {
 	public static int diff(int num1, int num2) {
 		return num1-num2;
 	}
+	public static int mul(int num1, int num2) {
+		return num1*num2;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Sum is "+add(10,20));
 		System.out.println("Difference is "+diff(100,20));
+		System.out.println("Product is "+mul(100,20));
 
 	}
 
