267dc3b0f7783a2741ba76b2aa72bab1ccf265ff
Srinidhi-Ravi <srini3998@gmailcom>
2020-12-21 14:58:52 +0530

    add and sub methods.

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -5,10 +5,14 @@ public class Calc {
 	public static int add(int num1, int num2) {
 		return num1+num2;
 	}
+	public static int diff(int num1, int num2) {
+		return num1-num2;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(add(10,20));
+		System.out.println("Sum is "+add(10,20));
+		System.out.println("Difference is "+diff(100,20));
 
 	}
 
